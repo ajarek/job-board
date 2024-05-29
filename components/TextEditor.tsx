be2dbdc84eb97835e5eacb2b88dec341336987b8
ajarek2101 <ajarek2101@gmail.com>
2024-05-29 21:21:17 +0000

    added text center

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState, useRef, useEffect } from 'react'
 import { Button } from './ui/button'
+import { Bold, Italic, Underline, Strikethrough, CaseUpper, CaseSensitive, AlignCenter } from 'lucide-react'
 
 const TextEditor = () => {
   const editorRef = useRef<HTMLTextAreaElement>(null)
@@ -12,11 +13,12 @@ const TextEditor = () => {
   const [strikethrough, setStrikethrough] = useState<boolean>(false)
   const [uppercase, setUppercase] = useState<boolean>(false)
   const [capitalize, setCapitalize] = useState<boolean>(false)
+  const [center, setCenter] = useState<boolean>(false)
 
 
   useEffect(() => {
     const text = document.querySelector('textarea')
-
+    
 
     bold ? text?.classList.add('font-bold') : text?.classList.remove('font-bold')
     italic ? text?.classList.add('italic') : text?.classList.remove('italic')
@@ -24,18 +26,21 @@ const TextEditor = () => {
     strikethrough ? text?.classList.add('line-through') : text?.classList.remove('line-through')
     uppercase ? text?.classList.add('uppercase') : text?.classList.remove('uppercase')
     capitalize ? text?.classList.add('capitalize') : text?.classList.remove('capitalize')
+    center ? text?.classList.add('text-center') : text?.classList.remove('text-center')
 
-  }, [bold, italic, underline, strikethrough, uppercase, capitalize])
+  }, [bold, italic, underline, strikethrough, uppercase, capitalize, center])
 
   return (
     <div>
       <div className='flex flex-wrap gap-2'>
-        <Button onClick={() => setBold(!bold)}>Bold</Button>
-        <Button onClick={() => setItalic(!italic)}>Italic</Button>
-        <Button onClick={() => setUnderline(!underline)}>Underline</Button>
-        <Button onClick={() => setStrikethrough(!strikethrough)}>Strikethrough</Button>
-        <Button onClick={() => setUppercase(!uppercase)}>Uppercase</Button>
-        <Button onClick={() => setCapitalize(!capitalize)}>Capitalize</Button>
+        <Button onClick={() => setBold(!bold)}><Bold /></Button>
+        <Button onClick={() => setItalic(!italic)}><Italic /></Button>
+        <Button onClick={() => setUnderline(!underline)}><Underline /></Button>
+        <Button onClick={() => setStrikethrough(!strikethrough)}><Strikethrough /></Button>
+        <Button onClick={() => setUppercase(!uppercase)}><CaseUpper /></Button>
+        <Button onClick={() => setCapitalize(!capitalize)}><CaseSensitive /></Button>
+        <Button onClick={() => setCenter(!center)}><AlignCenter /></Button>
+
 
       </div>
       <textarea ref={editorRef} name="description" id="" rows={5} placeholder='Enter your text here' className='w-full p-4 rounded-lg'></textarea>
